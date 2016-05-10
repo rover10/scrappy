4a61ba15ad1d4c2fdbb823bfdc2ba967080edd79
rakesh <rakesh.r1303@gmail.com>
2016-05-11 01:11:04 +0530

    found all mobile phone pages

--- scrappy.py
+++ scrappy.py
@@ -12,14 +12,31 @@ for line in response:
         found = True
         
     brandPage = re.search(r"href=", line)
-    if found is True and brandPage is not None:
+    if found and brandPage is not None:
         comUrl = line[ brandPage.end(): re.search(r'>',line[brandPage.end():]).start() + brandPage.end()]
         compList.append(comUrl)
-    if found is True and re.search(r"</table>", line) is not None:
+    if found and re.search(r"</table>", line) is not None:
         print "OUT!"
         break
     
 print compList
+mobUriList = []
 for uri in compList:
     response =  urllib2.urlopen("http://www.gsmarena.com/" + uri)
-    print(response)
+    #print(response)
+    found = False
+    for line in response:
+        if re.search(r"nav-pages", line) is not None:
+            found=True
+        mobPage = re.search(r"href=", line)
+        if found and mobPage is not None:
+            mobUri = line[ mobPage.end(): re.search(r'>',line[mobPage.end():]).start() + mobPage.end()]    
+            mobUriList.append(mobUri)
+        if found and re.search(r"</div>", line):
+            break
+        
+            
+print mobUriList
+#reads all 
+mobUrlList.extend(compList)
+
